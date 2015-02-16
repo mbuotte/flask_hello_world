dbe01c4dd2cea66d413273da5a993863de81043b
Maureen Buotte <mbuotte@eagleaccess.com>
2015-02-16 20:34:38 +0000

    Init

--- hello_world.py
+++ hello_world.py
@@ -1,37 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask (__name__)
 
+@app.template_filter()
+def datetimefilter(value, format='%Y/%m/%d %H:%M'):
+    """Convert a datetime to a different format."""
+    return value.strftime(format)
+
 @app.route("/hello")
 def hello_world():
-  return "Hello World!"
+  return render_template('template.html', my_string="Hello World!", current_time=datetime.now())
 
 @app.route("/hello/<name>")
 def hello_person(name):
- ## return "Hello {}!".format(name.title())
-  html = """
-  <h1>
-    Hello {}!
-  </h1>
-  <p>
-    Here's a picture of a kitten.  Awww ..
-  </p>
-  <img src="http://placekitten.com/g/200/300">
-"""
-  return html.format(name.title())
+  return render_template('template.html', my_string="Hello " + name + " !", current_time=datetime.now())
 
 @app.route("/jedi/<fname>/<lname>")
 def hello_jedi(fname,lname):
   jname = lname[0:3] + fname[0:2]
-  html = """
-  <h1>
-    Hello {0} {1}!
-    Your Jedi Name is:  {2}
-  </h1>
-"""
-  return html.format(fname.title(), lname.title(), jname.title())
+  full_string = "Your Jedi Name is: " + jname
+  return render_template('template.html', my_string=full_string, current_time=datetime.now())
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=8080)
+  app.run(debug=True, host="0.0.0.0", port=8080)
   
   
